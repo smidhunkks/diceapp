eb5779ccc4f7d5dc18bcce1c7d69ef16cf214181
midhun <jmsmidhunkrishna@gmail.com>
2020-05-15 14:43:08 +0530

    removed debug tag

--- main.dart
+++ main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
